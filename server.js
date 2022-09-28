4f34a21e733ad05ca5a356900d0c3af5c9ab7d39
jonrob <jonrob08@gmail.com>
2022-09-27 20:07:05 -0700

    updated GET route

--- server.js
+++ server.js
@@ -173,7 +173,9 @@ app.get("/profile", isLoggedIn, (req, res) => {
       res.render("profile", { req, id, name, email, playlist: playlist });
     })
     .catch((error) => {
-      console.log(error)
+      console.log('*******************************************************');
+      console.log(error);
+      console.log('*******************************************************');
       res.status(400).render("404");
     });
 });
